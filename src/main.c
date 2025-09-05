43e49a4c32ca29a5be5ffa1242bf2c50ac8accd5
chrid97 <cgoncal12@gmail.com>
2025-09-05 02:28:03 -0400

    more tilemap stuff

--- src/main.c
+++ src/main.c
@@ -1,11 +1,14 @@
 #include "raylib.h"
 #include <assert.h>
-#include <math.h>
 #include <stdbool.h>
 #include <stdint.h>
 
 #define VIRTUAL_WIDTH 800
 #define VIRTUAL_HEIGHT 450
+#define TILE_WIDTH 50
+#define TILE_HEIGHT 50
+#define TILE_COUNT_X 16
+#define TILE_COUNT_Y 9
 
 typedef struct {
   Vector2 position;
@@ -15,20 +18,40 @@ typedef struct {
   char *inventory;
 } Entity;
 
-#define TILE_HEIGHT = 9;
-#define TILE_WIDTH = 16;
+typedef struct {
+  int count_x;
+  int count_y;
 
-int TILE_MAP[9][16] = {
+  int upper_left_x;
+  int upper_left_y;
+  int tile_width;
+  int tile_height;
+} TileMap;
+
+int TILE_MAP[TILE_COUNT_Y][TILE_COUNT_X] = {
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 };
+
+bool is_tilemap_point_empty(TileMap *tile_map, int test_x, int test_y) {
+  int player_tile_x = (int)(test_x / tile_map->tile_width);
+  int player_tile_y = (int)(test_y / tile_map->tile_height);
+
+  if (player_tile_x >= 0 && player_tile_x < tile_map->count_x &&
+      player_tile_y >= 0 && player_tile_y < tile_map->count_y) {
+    int tilemap = TILE_MAP[player_tile_y][player_tile_x];
+    return tilemap == 0;
+  }
+
+  return false;
+}
 
 void test(char *text) {
   DrawText(text, GetScreenWidth() / 2, GetScreenHeight() / 2, 100, RED);
@@ -41,7 +64,8 @@ int main(void) {
   Entity player = {
       // .position = {.x = GetScreenWidth() / 2.0f,
       //              .y = GetScreenHeight() - (GetScreenHeight() / 4.0f)},
-      .position = {.x = 0, .y = 0},
+      .position = {.x = 50, .y = 50},
+      .velocity = {.x = 0, .y = 0},
       .height = 32,
       .width = 32};
 
@@ -59,39 +83,57 @@ int main(void) {
     // --------------- //
     // ---- Input ---- //
     // --------------- //
+    player.velocity.y = 0;
+    player.velocity.x = 0;
     if (IsKeyDown(KEY_W)) {
+      player.velocity.y = -1.0f;
     }
     if (IsKeyDown(KEY_S)) {
+      player.velocity.y = 1.0f;
     }
     if (IsKeyDown(KEY_A)) {
+      player.velocity.x = -1.0f;
     }
     if (IsKeyDown(KEY_D)) {
+      player.velocity.x = 1.0f;
     }
+    player.velocity.x *= 150.0f;
+    player.velocity.y *= 150.0f;
 
     // ---------------- //
     // ---- Update ---- //
     // ---------------- //
+    float new_player_x = player.position.x + player.velocity.x * dt;
+    float new_player_y = player.position.y + player.velocity.y * dt;
+
+    TileMap tile_map = {.count_x = TILE_COUNT_X,
+                        .count_y = TILE_COUNT_Y,
+                        .tile_width = TILE_WIDTH,
+                        .tile_height = TILE_HEIGHT};
+
+    if (is_tilemap_point_empty(&tile_map, new_player_x, new_player_y)) {
+      player.position.x = new_player_x;
+      player.position.y = new_player_y;
+    }
 
     // ---------------- //
     // ----- Draw ----- //
     // ---------------- //
     BeginDrawing();
+    ClearBackground(BLACK);
 
-    for (int y = 0; y < 9; y++) {
-      for (int x = 0; x < 16; x++) {
-        int tile_height = 50;
-        int tile_width = 50;
+    for (int y = 0; y < TILE_COUNT_Y; y++) {
+      for (int x = 0; x < TILE_COUNT_X; x++) {
         int tile = TILE_MAP[y][x];
         if (tile == 1) {
-          DrawRectangle(x * tile_width, y * tile_height, tile_width,
-                        tile_height, WHITE);
+          DrawRectangle(x * TILE_WIDTH, y * TILE_HEIGHT, TILE_WIDTH,
+                        TILE_HEIGHT, WHITE);
         }
       }
     }
 
-    DrawCircle(player.position.x + 25, player.position.y + 25, 20, BLUE);
-
-    ClearBackground(BLACK);
+    DrawRectangle(player.position.x, player.position.y, player.width,
+                  player.height, BLUE);
 
     EndDrawing();
   }
