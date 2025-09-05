496ef6cb9dc2303fc9d1f1568cebc433f9e46642
Christopher Goncalves <cgoncal12@gmail.com>
2025-09-04 21:12:45 -0700

    tile map stuff

--- src/main.c
+++ src/main.c
@@ -15,17 +15,33 @@ typedef struct {
   char *inventory;
 } Entity;
 
+#define TILE_HEIGHT = 9;
+#define TILE_WIDTH = 16;
+
+int TILE_MAP[9][16] = {
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+};
+
 void test(char *text) {
   DrawText(text, GetScreenWidth() / 2, GetScreenHeight() / 2, 100, RED);
 }
 
 int main(void) {
-  InitWindow(800, 450, "Topspin");
+  InitWindow(800, 450, "Tradebinder");
   SetTargetFPS(60);
 
   Entity player = {
-      .position = {.x = GetScreenWidth() / 2.0f,
-                   .y = GetScreenHeight() - (GetScreenHeight() / 4.0f)},
+      // .position = {.x = GetScreenWidth() / 2.0f,
+      //              .y = GetScreenHeight() - (GetScreenHeight() / 4.0f)},
+      .position = {.x = 0, .y = 0},
       .height = 32,
       .width = 32};
 
@@ -60,6 +76,21 @@ int main(void) {
     // ----- Draw ----- //
     // ---------------- //
     BeginDrawing();
+
+    for (int y = 0; y < 9; y++) {
+      for (int x = 0; x < 16; x++) {
+        int tile_height = 50;
+        int tile_width = 50;
+        int tile = TILE_MAP[y][x];
+        if (tile == 1) {
+          DrawRectangle(x * tile_width, y * tile_height, tile_width,
+                        tile_height, WHITE);
+        }
+      }
+    }
+
+    DrawCircle(player.position.x + 25, player.position.y + 25, 20, BLUE);
+
     ClearBackground(BLACK);
 
     EndDrawing();
